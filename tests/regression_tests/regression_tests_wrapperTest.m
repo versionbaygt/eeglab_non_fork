ca2272d9f3069a593a0603a1e9d45db9f5f7948a
Jorik Caljouw <jorik@versionbay.com>
2022-02-02 06:26:18 +0100

    Add generated wrapperTest functions

---
+++ tests/regression_tests/regression_tests_wrapperTest.m
@@ -0,0 +1,9 @@
+function tests = regression_tests_wrapperTest
+tests = functiontests(localfunctions);
+
+
+function test_t_finputcheck(~)
+t_finputcheck
+
+function test_t_statcond(~)
+t_statcond
---
+++ tests/tests_wrapperTest.m
@@ -0,0 +1,12 @@
+function tests = tests_wrapperTest
+tests = functiontests(localfunctions);
+
+
+function test_readcontsamplefile(~)
+readcontsamplefile
+
+function test_readepochsamplefile(~)
+readepochsamplefile
+
+function test_runtest_setenvironment(~)
+runtest_setenvironment
